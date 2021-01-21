864b01eaf3f826e4d8a69a06327a9d2219ff801c
S Z Haider <61172135+zain1320@users.noreply.github.com>
2021-01-21 15:09:58 -0500

    Update twitter.py

--- TwitterAutoBaba/twitter.py
+++ TwitterAutoBaba/twitter.py
@@ -1,20 +1,17 @@
 import tweepy
 import time
-
-auth = tweepy.OAuthHandler('ypjbVYGsEaCnAY33MSjVunBct','XRQkdPL6Jc8Cr1JrfWsP1lNtcQlUTDbtgV1n5rG22oz1di0aYA')
+auth = tweepy.OAuthHandler('ypjbVYGsEaCnAY33MSjVunBct','XRQkdPL6Jc8Cr1JrfWsP1lNtcQlUTDbtgV1n5rG22oz1di0aYA') //Old tokens
 auth.set_access_token('3457319180-B67mrUicV3Bto1uc8lvOEYChHbkv0S5hORMSAj2','9NwG2aYWAJN9JAWTZ7ToZYoMM0BDignzuFkoUi7UtkH7y')
-
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
 user = api.me()
 
-search = 'Football'
-nrTweets = 200
+search = 'Football' //Football can be replaced with the keyboard we want to look for in a tweet 
+nrTweets = 200 //Limit to look for tweets 
 for tweet in tweepy.Cursor(api.search,search).items(nrTweets):
     try:
         print('Tweet Liked')
         tweet.favorite()
-        time.sleep(5)
+        time.sleep(5) //A pause period provided in between liking of tweets
     except tweepy.TweepError as e:
         print(e.reason)
     except StopIteration:
